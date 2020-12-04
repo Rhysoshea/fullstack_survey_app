9428b99a9f75f6b60527484cd0197870f8d8d232
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-12-04 14:23:00 +0900

    utilities for maintaining the session

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-
+import jwt_decode from 'jwt-decode';
+import store from '../store/store';
+import { initiateGetProfile } from '../actions/profile';
+import { signIn } from '../actions/auth';
+import { history } from '../router/AppRouter';
 
 export const validateFields = (fieldsToValidate) => {
     return fieldsToValidate.every((field) => Object.values(field)[0] !== '');
@@ -14,4 +18,26 @@ export const setAuthHeader = () => {
 
 export const removeAuthHeader = () => {
     delete axios.defaults.headers.common['Authorization'];
+};
+
+export const maintainSession = () => {
+    const user_token = localStorage.getItem('user_token');
+    if (user_token) {
+        const decoded = jwt_decode(user_token);
+        updateStore(decoded);
+    } else {
+        history.push('/');
+    }
+};
+
+export const updateStore = (user) => {
+    const { userid, email } = user;
+    store.dispatch(
+        signIn({
+            userid,
+            email,
+            token: localStorage.getItem('user_token')
+        })
+    );
+    store.dispatch(initiateGetProfile(email));
 };
